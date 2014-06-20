32a7d1d6be27d1b537ac5ae0d0adda0f37b35f00
Supun Nakandala <supun.nakandala@gmail.com>
2014-06-20 16:40:29 +0530

    added more php test samples

---
+++ clients/php-client/client-samples/activateUser.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Airavata\UserAPI\Client\Samples;
+
+$GLOBALS['THRIFT_ROOT'] = '../lib/Thrift/';
+require_once $GLOBALS['THRIFT_ROOT'] . 'Transport/TTransport.php';
+require_once $GLOBALS['THRIFT_ROOT'] . 'Transport/TSocket.php';
+require_once $GLOBALS['THRIFT_ROOT'] . 'Protocol/TProtocol.php';
+require_once $GLOBALS['THRIFT_ROOT'] . 'Protocol/TBinaryProtocol.php';
+require_once $GLOBALS['THRIFT_ROOT'] . 'Exception/TException.php';
+require_once $GLOBALS['THRIFT_ROOT'] . 'Exception/TApplicationException.php';
+require_once $GLOBALS['THRIFT_ROOT'] . 'Exception/TProtocolException.php';
+require_once $GLOBALS['THRIFT_ROOT'] . 'Exception/TTransportException.php';
+require_once $GLOBALS['THRIFT_ROOT'] . 'Base/TBase.php';
+require_once $GLOBALS['THRIFT_ROOT'] . 'Type/TType.php';
+require_once $GLOBALS['THRIFT_ROOT'] . 'Type/TMessageType.php';
+require_once $GLOBALS['THRIFT_ROOT'] . 'Factory/TStringFuncFactory.php';
+require_once $GLOBALS['THRIFT_ROOT'] . 'StringFunc/TStringFunc.php';
+require_once $GLOBALS['THRIFT_ROOT'] . 'StringFunc/Core.php';
+
+$GLOBALS['AIRAVATA_ROOT'] = '../lib/Airavata/';
+require_once $GLOBALS['AIRAVATA_ROOT'] . 'UserAPI/UserAPI.php';
+require_once $GLOBALS['AIRAVATA_ROOT'] . 'UserAPI/UserAPI.php';
+
+use Airavata\UserAPI\Error\UserAPISystemException;
+use Airavata\UserAPI\Error\InvalidRequestException;
+use Airavata\UserAPI\Error\AuthorizationException;
+use Airavata\UserAPI\Error\AuthenticationException;
+use Airavata\UserAPI\Error\TimedOutException;
+
+use Airavata\UserAPI\Client\AiravataClientFactory;
+use Thrift\Protocol\TBinaryProtocol;
+use Thrift\Transport\TSocket;
+use Airavata\UserAPI\UserAPIClient;
+
+$userapiconfig = parse_ini_file("userapi-client-properties.ini");
+
+$transport = new TSocket($userapiconfig['USERAPI_SERVER'], $userapiconfig['USERAPI_PORT']);
+$transport->setRecvTimeout($userapiconfig['USERAPI_TIMEOUT']);
+
+$protocol = new TBinaryProtocol($transport);
+$transport->open();
+$client = new UserAPIClient($protocol);
+
+try
+{
+    $token = $client->adminLogin($userapiconfig['ADMIN_USERNAME'],$userapiconfig['ADMIN_PASSWORD']);
+    if($token !== null){
+        //User name must be a non null string with following format, [a-zA-Z0-9._-|//]
+        $client->createNewUser("phpTestUser","testUserPwd", $token);
+
+        //By default a user is created as an active user
+        if($client->isActiveUser("phpTestUser", $token)){
+            print "New user is created as an active user\n";
+            $client->deactivateUser("phpTestUser", $token);
+            if(!$client->isActiveUser("phpTestUser", $token)){
+                print "New user is deactivated successfully\n";
+                $client->activateUser("phpTestUser", $token);
+                if($client->isActiveUser("phpTestUser", $token)){
+                    print "New user is re-activated successfully\n";
+                    $client->removeUser("phpTestUser", $token);
+                }else{
+                    print "Error! New user was unable to re-activate\n";
+                    $client->removeUser("phpTestUser", $token);
+                    exit;
+                }
+            }else{
+                print "Error! New user was unable to deactivate\n";
+                $client->removeUser("phpTestUser", $token);
+                exit;
+            }
+        }else{
+            print "Error! New user was not created as an active user\n";
+            $client->removeUser("phpTestUser", $token);
+            exit;
+        }
+    }else{
+        print "Invalid credential for the Admin" . "\n";
+    }
+}
+catch (TException $texp)
+{
+    print "Exception: " . $texp->getMessage()."\n";
+}
